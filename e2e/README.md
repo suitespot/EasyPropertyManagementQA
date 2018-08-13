6ea833be95d6db81320770ea870a3e372adb17c1
Will Fairclough <will.fairclough@suitespottechnology.com>
2018-08-13 15:38:05 -0400

    Added api e2e test to add a new property, upgrade to npm@6.3.0

--- e2e/README.md
+++ e2e/README.md
@@ -5,7 +5,7 @@
 The following is a list of dependencies and their versions that must be installed to use this project.
 
 * node@8.9.4 [nvm](https://github.com/creationix/nvm) is a great tool to manage multiple installations of node
-* npm@5.6.0
+* npm@6.3.0
 * ng@1.7.4 [Angular CLI](https://cli.angular.io/)
 
 ## Get It Running
--- e2e/e2e.api/properties.e2e.spec.ts
+++ e2e/e2e.api/properties.e2e.spec.ts
@@ -2,10 +2,21 @@ import { requestBuilder } from './request.builder.promise';
 
 describe('/api/properties', () => {
 
+  it('should add properties', async () => {
+    const response = await requestBuilder('http://localhost:3000/api/properties')
+      .body({
+        name: 'Test Property',
+        address: '23 Baker st.',
+      })
+      .resolveWithFullResponse()
+      .post();
+    expect(response.statusCode).toBe(200);
+  });
+
   it('should get properties', async () => {
     const response = await requestBuilder('http://localhost:3000/api/properties')
       .get();
-    expect(response.length).toBeGreaterThan(0);
+    expect(response.length).toBe(1);
   });
 
 });
